250b8a9fdba1bf3abb45dba71b0947cafb3fd4ac
4919545484946757 <92537738+4919545484946757@users.noreply.github.com>
2024-10-18 13:37:45 +0800

    more function
    some else

--- 项目1/assets/js/login.js
+++ 项目1/assets/js/login.js
@@ -1,17 +1,40 @@
 function check() {
-    var username = document.forms["loginform"]["username"].value;
-    var password = document.forms["loginform"]["password"].value;
-    var account = document.forms["loginform"]["account"].value;
-    if (username == "" || password == "" || account == "") {
-        alert("请填写完整信息");
-        var logined = false;
-        return false;
-    }
-    else {
-        localStorage.setItem("accountdata", account);
-        localStorage.setItem("usernamedata", username);
-        localStorage.setItem("logined", "true");
-        window.location.href = "sons-1/transfer.html";
-        return false;
+    if (loginwin.classList.contains("active")){
+        var username = document.forms["regform"]["username"].value;
+        var password = document.forms["regform"]["password"].value;
+        var account = document.forms["regform"]["account"].value;
+        
+        if (username == "" || password == "" || account == "") {
+            alert("请填写完整信息");
+            var logined = false;
+            return false;
+        }
+        else {
+            localStorage.setItem("accountdata", account);
+            localStorage.setItem("usernamedata", username);
+            localStorage.setItem("logined", "true");
+            window.location.href = "sons-1/transfer.html";
+            return false;
+        }
+    }else{
+        var username = document.forms["loginform"]["username"].value;
+        var password = document.forms["loginform"]["password"].value;
+        var account = document.forms["loginform"]["account"].value;
+        
+        if (username == "" || password == "" || account == "") {
+            alert("请填写完整信息");
+            var logined = false;
+            return false;
+        }
+        else {
+            localStorage.setItem("accountdata", account);
+            localStorage.setItem("usernamedata", username);
+            localStorage.setItem("logined", "true");
+            window.location.href = "sons-1/transfer.html";
+            return false;
+        }
     }
 }
+
+
+
